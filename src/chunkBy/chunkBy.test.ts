73f2109ac80edac64b56001887039e735fa3982e
Ashley Towner <git@ashtowner.com>
2022-12-19 13:41:19 +1030

    MICRO-30: ChunkBy

---
+++ src/chunkBy/chunkBy.test.ts
@@ -0,0 +1,13 @@
+import chunkBy from './chunkBy';
+
+describe('chunkBy', () => {
+    it('should split up an array into sub-arrays of a certain size', () => {
+        const arr = [1, 2, 3, 4];
+        expect(chunkBy(arr, 2)).toEqual([[1, 2], [3, 4]]);
+    });
+
+    it('should put leftover items in the last chunk', () => {
+        const arr = [1, 2, 3, 4, 5];
+        expect(chunkBy(arr, 2)).toEqual([[1, 2], [3, 4], [5]]);
+    })
+})
---
+++ src/chunkBy/chunkBy.ts
@@ -0,0 +1,11 @@
+/**
+ * Split an array into chunks of a certain size
+ * @param source the array to split up
+ * @param chunkSize the size of each chunk. (The final chunk will be whatever is remaining)
+ */
+export default function chunkBy<T>(source: T[], chunkSize: number) {
+    const numberOfChunks = Math.ceil(source.length / chunkSize);
+    return Array.from(new Array(numberOfChunks), (_, i) => {
+        return source.slice(i * chunkSize, (i + 1) * chunkSize);
+    })
+}

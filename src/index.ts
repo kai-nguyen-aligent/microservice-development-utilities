7a94552c35403f3c30f0a3418996310881bbe994
Ashley Towner <git@ashtowner.com>
2022-12-22 13:39:14 +1030

    MICRO-30: Fixed structure so that it can be used as an NPM package

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import chunkBy from '@/chunkBy/chunkBy';
-import remap, { Remap, ObjectMap } from '@/remap/remap';
-import retryWrapper, { RetryConfig } from '@/retryWrapper/retryWrapper';
+import chunkBy from './chunkBy/chunkBy';
+import remap, { Remap, ObjectMap } from './remap/remap';
+import retryWrapper, { RetryConfig } from './retryWrapper/retryWrapper';
 
 export {
   chunkBy,

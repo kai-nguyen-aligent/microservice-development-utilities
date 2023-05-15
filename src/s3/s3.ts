d06bfad58e1a8fee8b973a7db9d358199e7dd095
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2023-05-15 18:58:59 +0930

    MICRO-30: Reimplement s3 and SSM functions and update tests

--- src/s3/s3.ts
+++ src/s3/s3.ts
@@ -1,11 +1,16 @@
-import { S3 } from 'aws-sdk';
+import {
+  DeleteObjectCommand,
+  GetObjectCommand,
+  GetObjectCommandInput,
+  PutObjectCommand,
+  S3Client
+} from '@aws-sdk/client-s3';
 import hash from 'object-hash';
-
 import chunkBy from '../chunkBy/chunkBy';
 
 /** A data access object for an S3 bucket */
 class S3Dao {
-  private s3: S3;
+  private s3: S3Client;
   private bucket: string;
 
   /**
@@ -13,7 +18,7 @@ class S3Dao {
    */
   constructor(bucket: string) {
     this.bucket = bucket;
-    this.s3 = new S3();
+    this.s3 = new S3Client({});
   }
 
   /**
@@ -25,19 +30,20 @@ class S3Dao {
   public async storeData<T>(
     data: T,
     name?: string
-  ): Promise<S3.GetObjectRequest> {
+  ): Promise<GetObjectCommandInput> {
     if (typeof data === 'undefined') {
       throw new Error('data is undefined');
     }
-    const getObject: S3.GetObjectRequest = {
+
+    const getObject: GetObjectCommandInput = {
       Bucket: this.bucket,
       Key: name || hash(data as any)
     };
 
-    await this.s3.putObject({
+    await this.s3.send(new PutObjectCommand({
       ...getObject,
       Body: JSON.stringify(data)
-    }).promise();
+    }));
 
     return getObject;
   }
@@ -50,7 +56,7 @@ class S3Dao {
    */
   public async storeChunked<T extends any[]>(data: T, chunkSize: number) {
     const chunks = chunkBy(data, chunkSize);
-    return await Promise.all(chunks.map(chunk => this.storeData(chunk)));
+    return Promise.all(chunks.map(chunk => this.storeData(chunk)));
   }
 
   /**
@@ -58,8 +64,8 @@ class S3Dao {
    * @param objectDetails the object which describes the location of the object
    * @returns the body of the object
    */
-  public async fetchData<T>(objectDetails: S3.GetObjectRequest): Promise<T> {
-    const data = await this.s3.getObject(objectDetails).promise();
+  public async fetchData<T>(objectDetails: GetObjectCommandInput): Promise<T> {
+    const data = await this.s3.send(new GetObjectCommand(objectDetails));
     return JSON.parse(data.Body.toString());
   }
 
@@ -67,7 +73,7 @@ class S3Dao {
    * Generator to fetch chunked data, chunk by chunk
    * @param chunks the list of object chunks
    */
-  public async *fetchChunks<T>(chunks: S3.GetObjectRequest[]) {
+  public async *fetchChunks<T>(chunks: GetObjectCommandInput[]) {
     for (let i = 0; i < chunks.length; i++) {
       const chunk: T = await this.fetchData<T>(chunks[i]);
 
@@ -87,8 +93,8 @@ class S3Dao {
    * Delete an object from the S3 bucket
    * @param objectDetails the object to delete
    */
-  public async deleteData(objectDetails: S3.GetObjectRequest) {
-    return await this.s3.deleteObject(objectDetails).promise();
+  public async deleteData(objectDetails: GetObjectCommandInput) {
+    return this.s3.send(new DeleteObjectCommand(objectDetails));
   }
 }
 

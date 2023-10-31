d7c57e30748aa91062fa05e4f43300550bbca35c
chamal <chamal.chamikara@aligent.com.au>
2023-10-31 11:08:28 +1030

    MICRO-67: If requested param is doesn't exist in AWS it will return as undefined so including undefined in return type

--- src/fetchSsmParams/fetchSsmParams.ts
+++ src/fetchSsmParams/fetchSsmParams.ts
@@ -12,14 +12,14 @@ const ssm = new SSMClient({});
  * @param param key of the parameter to fetch
  */
 async function fetchSsmParams(param: string):
-  Promise<Parameter>;
+  Promise<Parameter | undefined>;
 
 /**
  * Fetch a list of SSM parameters
  * @param params list of parameter keys to fetch
  */
 async function fetchSsmParams(...params: string[]):
-  Promise<Parameter[]>;
+  Promise<Parameter[] | undefined[]>;
 
 /**
  * Fetch SSM Parameters

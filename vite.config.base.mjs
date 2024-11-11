ddc59fd619021f7c1403aa9f794c6796117277d3
Kai Nguyen <kai.nguyen@aligent.com.au>
2024-11-11 20:04:49 +1100

    MI-39: Working Nx template

---
+++ vite.config.base.mjs
@@ -0,0 +1,29 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export const viteBaseConfig = defineConfig({
+    plugins: [nxViteTsPaths()],
+
+    test: {
+        globals: true,
+        watch: false,
+        environment: 'node',
+        passWithNoTests: true,
+        reporters: ['default'],
+        coverage: {
+            provider: 'v8',
+            exclude: ['node_modules/', '**/types', '*.mjs'],
+            thresholds: {
+                branches: 80,
+                functions: 80,
+                lines: 80,
+                statements: 80,
+            },
+        },
+        include: [
+            'src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+            'tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+        ],
+    },
+});

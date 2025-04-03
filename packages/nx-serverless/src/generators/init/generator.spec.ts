6267a53bb551b333f76622187389ab0d0abfd862
Kai Nguyen <kai.nguyen@aligent.com.au>
2025-04-03 22:36:15 +1100

    MI-198: Update plugin test

--- packages/nx-serverless/src/generators/init/generator.spec.ts
+++ packages/nx-serverless/src/generators/init/generator.spec.ts
@@ -9,7 +9,7 @@ describe('@aligent/nx-serverless:init', () => {
         tree = createTreeWithEmptyWorkspace();
     });
 
-    it('should add target defaults for test', async () => {
+    it('should add plugin configuration with default options', async () => {
         updateJson<NxJsonConfiguration>(tree, 'nx.json', json => {
             json.namedInputs ??= {};
             json.namedInputs.production = ['default'];
@@ -35,6 +35,96 @@ describe('@aligent/nx-serverless:init', () => {
                   "options": {},
                   "plugin": "@aligent/nx-serverless/plugin",
                 },
+              ],
+              "targetDefaults": {
+                "build": {
+                  "cache": true,
+                },
+                "lint": {
+                  "cache": true,
+                },
+              },
+            }
+        `);
+    });
+
+    it('should not overwrite existing plugin configuration', async () => {
+        updateJson<NxJsonConfiguration>(tree, 'nx.json', json => {
+            json.namedInputs ??= {};
+            json.namedInputs.production = ['default'];
+            json.plugins ??= [
+                {
+                    plugin: '@aligent/nx-serverless/plugin',
+                    options: {
+                        buildTargetName: 'build',
+                        deployTargetName: 'deploy',
+                        removeTargetName: 'remove',
+                    },
+                },
+            ];
+            return json;
+        });
+
+        await initGenerator(tree, {});
+
+        const nxJson = readNxJson(tree);
+
+        expect(nxJson).toMatchInlineSnapshot(`
+            {
+              "affected": {
+                "defaultBase": "main",
+              },
+              "namedInputs": {
+                "production": [
+                  "default",
+                ],
+              },
+              "plugins": [
+                {
+                  "options": {
+                    "buildTargetName": "build",
+                    "deployTargetName": "deploy",
+                    "removeTargetName": "remove",
+                  },
+                  "plugin": "@aligent/nx-serverless/plugin",
+                },
+              ],
+              "targetDefaults": {
+                "build": {
+                  "cache": true,
+                },
+                "lint": {
+                  "cache": true,
+                },
+              },
+            }
+        `);
+    });
+
+    it('should not overwrite existing short form plugin configuration', async () => {
+        updateJson<NxJsonConfiguration>(tree, 'nx.json', json => {
+            json.namedInputs ??= {};
+            json.namedInputs.production = ['default'];
+            json.plugins ??= ['@aligent/nx-serverless/plugin'];
+            return json;
+        });
+
+        await initGenerator(tree, {});
+
+        const nxJson = readNxJson(tree);
+
+        expect(nxJson).toMatchInlineSnapshot(`
+            {
+              "affected": {
+                "defaultBase": "main",
+              },
+              "namedInputs": {
+                "production": [
+                  "default",
+                ],
+              },
+              "plugins": [
+                "@aligent/nx-serverless/plugin",
               ],
               "targetDefaults": {
                 "build": {

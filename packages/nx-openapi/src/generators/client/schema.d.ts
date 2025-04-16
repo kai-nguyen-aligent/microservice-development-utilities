a55c2f0a3277920040eecea356db1972693c4d83
Kai Nguyen <kai.nguyen@aligent.com.au>
2025-04-17 00:26:16 +1000

    MI-204: Added back configPath as we'll support it in the future

--- packages/nx-openapi/src/generators/client/schema.d.ts
+++ packages/nx-openapi/src/generators/client/schema.d.ts
@@ -1,6 +1,7 @@
 export interface ClientGeneratorSchema {
     name: string;
     schemaPath: string;
+    configPath?: string;
     importPath?: string;
     skipValidate: boolean;
     override: boolean;

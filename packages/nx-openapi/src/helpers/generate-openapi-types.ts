6bfdc004d658ce140bfa7f5a8a7a381b5c3eb3fb
ryanrixxh <ryanmbr@protonmail.com>
2025-04-23 16:49:36 +0930

    MI-207 adding independent release tagging and version-plan

--- packages/nx-openapi/src/helpers/generate-openapi-types.ts
+++ packages/nx-openapi/src/helpers/generate-openapi-types.ts
@@ -19,6 +19,7 @@ export async function generateOpenApiTypes(
     typeDest: string
 ): Promise<void> {
     logger.info(`Generating types from ${schemaPath}`);
+    logger.info('testing new change!');
     const schema = tree.read(schemaPath);
 
     if (!schema) {
